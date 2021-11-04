7b317ecc17f043850f7084363f18e3f671c22abf
Irina Varushkina <73347907+Vistp@users.noreply.github.com>
2021-11-04 17:05:55 +0500

    Выносит total  и PositiveFeedback во вспомогательные методы

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
     // }
     
     clickButtonGood = () => {
-        // console.log('Клик на Good')
         this.setState(prevState => {
             return {
                 good: prevState.good + 1,
@@ -25,24 +24,25 @@ class App extends Component {
         });
     }
      clickButtonNeutral = () => {
-        //  console.log('Клик на Neutral')
          this.setState(prevState => ({
              neutral: prevState.neutral + 1,
          }));
     }
      clickButtonBad = () => {
-        //  console.log('Клик на Bad')
          this.setState(prevState => ({
              bad: prevState.bad + 1,
          }));
     }
-
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+    console.log("тотал");
+  }
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback()
+      ? Math.round(this.state.good * 100 / this.countTotalFeedback())
+      : 0;
+  }
     render() {
-
-    const total = this.state.good + this.state.neutral + this.state.bad;
-        // console.log(total);
-        const positiveFeedback = Math.round(this.state.good * 100 / total) + 0;
-        console.log(positiveFeedback);
         return (
         <div className="Feedback">
             <h2 className="Feedback__title">Please leave feedback</h2>
@@ -70,8 +70,8 @@ class App extends Component {
                     <li>Good: { this.state.good }</li>
                     <li>Neutral: { this.state.neutral }</li>
                     <li>Bad: { this.state.bad }</li>
-                    <li>Total: { total }</li>
-                    <li>Positive feedback: { positiveFeedback } %</li>
+                    <li>Total: { this.countTotalFeedback() }</li>
+                    <li>Positive feedback: { this.countPositiveFeedbackPercentage() } %</li>
                 </ul>
        </div>
         )   
